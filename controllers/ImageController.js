99dd97b143b63a39ef73e15c27df86fc9c14b70d
SaiCharanReddy_Maila <maila.s@northeastern.edu>
2023-03-31 13:52:30 -0400

    added logs

--- controllers/ImageController.js
+++ controllers/ImageController.js
@@ -34,7 +34,12 @@ module.exports = {
   create: ( async(req,res) => {
 
 
+  statsd.increment('endpoint_all');
+  statsd.increment('endpoint_Image_Post');
+
+
     if(req.headers.authorization === undefined){
+      logger.info("no auth header");
        return  res.sendStatus(401);
     }
 
@@ -51,6 +56,7 @@ module.exports = {
     console.log("i came here into the sql loop");
 
     if (list_1 === null  || list_1 === undefined){
+      logger.error("no user with the username");
         return res.status(400).json({ msg: " User Not Found" });
     }
     else{
@@ -66,6 +72,7 @@ const vUser = (username === name) ? true : false;
       const file = req.file;
 
       if(!req.file) {
+        logger.info("no file attached");
         return res.sendStatus(400);
       }
       const datatypes = ['image/jpeg','image/png','image/jpg'];
@@ -82,6 +89,7 @@ const vUser = (username === name) ? true : false;
         }
       });
       if(!b){
+        logger.info("file type mismatch");
         return res.sendStatus(400);
       }
       console.log(req.file.mimetype);
@@ -92,15 +100,18 @@ const vUser = (username === name) ? true : false;
 
        
         if(list_products === null  || list_products === undefined){
+          logger.error("no prod found");
             res.sendStatus(400);
         } 
         if(owner_user_id != list_products.UserId){
 
+          logger.error("access denied");
           return res.sendStatus(403);
 
         }
 
         if( req.file === "null"|| req.file === "undefined"){
+          logger.error("file not attached");
         res.sendStatus(400);}
 
         const productId = req.params.id;
@@ -116,21 +127,23 @@ const vUser = (username === name) ? true : false;
           s3_bucket_path: s3ObjectLocation,
         });
         if (i) {
+          logger.info("image created -->");
           return res.status(201).json(i);
         }
       }
       catch(e){
+        logger.error("creation error");
         return res.sendStatus(400);
       }
       
       const return_data = await Image.findOne({where : {s3_bucket_path : s3ObjectLocation}});
       
-
+      logger.info("no auth header");
       return res.sendStatus(201).json({msg:return_data});
     }
   
         else {
-            
+          logger.error("unauthorized");
             return res.status(401).json({msg: "not valid"});
         }
     }
@@ -141,8 +154,13 @@ const vUser = (username === name) ? true : false;
 
 getone: ( async(req,res) => {
 
+  statsd.increment('endpoint_all');
+  statsd.increment('endpoint_getImage');
+
+
 
   if(req.headers.authorization === undefined){
+    logger.error("no auth header");
      return  res.sendStatus(401);
   }
 
@@ -153,6 +171,7 @@ getone: ( async(req,res) => {
       var pass = decoded.split(':')[1];
   const list_1 = await User.findOne({ where: { username: name } });
   if (list_1 === null  || list_1 === undefined){
+    logger.error("no user found");
       return res.status(401).json({ msg: " User Not Found" });
   }
   else{
@@ -169,18 +188,21 @@ const vUser = (username === name) ? true : false;
       const list_products = await Product.findOne({ where: { id: req.params.productid } });
 
      if(!list_products){
+      logger.error("no prod found");
       return res.sendStatus(400);
      }
       if(list_products === "null"  || list_products === "undefined"){
           return res.sendStatus(400);
       }
       if(owner_user_id != list_products.UserId){
+        logger.error("forbidden id level check failed for prod");
        return res.sendStatus(403);
       }
       const image_data =  await Image.findOne({where: {id:req.params.id}});
       console.log(image_data);
 
       if(!image_data){
+        logger.error("no image with id");
         return res.sendStatus(400);
       }
       if(image_data === "null"|| image_data == "undefined" || typeof image_data === "undefined"|| typeof image_data === "null" ){
@@ -188,8 +210,10 @@ const vUser = (username === name) ? true : false;
       }
       
       if(image_data.ProductId != req.params.productid){
-       return  res.sendStatus(403);
+        logger.error("access prod level check ---> forbidden");
+        return  res.sendStatus(403);
       }
+      logger.info("req successful -->");
      return  res.status(200).json({image_data});
 
 
@@ -202,7 +226,8 @@ const vUser = (username === name) ? true : false;
   }
 
       else {
-          
+
+         logger.error("un authorized user check");  
           return res.status(401).json({msg: "not valid"});
       }
   }
@@ -212,8 +237,12 @@ const vUser = (username === name) ? true : false;
 
 getAll:  ( async(req,res) => {
 
+  statsd.increment('endpoint_all');
+  statsd.increment('endpoint_getAllImage');
+
 
   if(req.headers.authorization === undefined){
+    logger.error("no auth header");
      return  res.sendStatus(401);
   }
 
@@ -240,6 +269,7 @@ const vUser = (username === name) ? true : false;
       const list_products = await Product.findOne({ where: { id: req.params.productid } });
 
       if(!list_products){
+        logger.error("empty prod");
         return res.sendStatus(400);
       }
 
@@ -247,16 +277,19 @@ const vUser = (username === name) ? true : false;
           return res.sendStatus(400);
       }
       if(owner_user_id != list_products.UserId){
+        logger.error("access prod level check ---> forbidden");
        return res.sendStatus(403);
       }
       const image_data =  await Image.findAll({where: {ProductId:req.params.productid}});
       console.log(image_data);
       if(!image_data) {
+        logger.error("no iamges to find -->");
         return res.sendStatus(400);
       }
       if(typeof image_data === "null"|| typeof image_data === "undefined" || image_data === "null"|| image_data == "undefined" ){
         return res.sendStatus(400);
       }
+      logger.info("all images ---> found");
      return  res.status(200).json({image_data});
 
 
@@ -269,7 +302,8 @@ const vUser = (username === name) ? true : false;
   }
 
       else {
-          
+
+        logger.error("user level auth failed");  
           return res.status(401).json({msg: "not valid"});
       }
   }
@@ -278,8 +312,12 @@ const vUser = (username === name) ? true : false;
 }),
 delete: ( async(req,res) => {
 
+  statsd.increment('endpoint_all');
+  statsd.increment('endpoint_deleteImage');
+
 
   if(req.headers.authorization === undefined){
+    logger.error("no auth header");
      return  res.sendStatus(401);
   }
 
@@ -345,6 +383,7 @@ const vUser = (username === name) ? true : false;
           if (!d_image) {
             return res.sendStatus(400);
           } 
+          logger.info("successful deletion");
             return res.sendStatus(204);
           
         }
@@ -354,7 +393,8 @@ const vUser = (username === name) ? true : false;
   }
 
       else {
-          
+
+         logger.error("access prod level check --->");  
           return res.status(401).json({msg: "not valid"});
       }
   }
@@ -363,8 +403,12 @@ const vUser = (username === name) ? true : false;
 }),
 deleteAll : ( async(req,res) => {
 
+  statsd.increment('endpoint_all');
+  statsd.increment('endpoint_deleteAllImage');
+
 
   if(req.headers.authorization === undefined){
+    logger.error("no auth header");
      return  res.sendStatus(401);
   }
 
@@ -448,8 +492,10 @@ const vUser = (username === name) ? true : false;
             return res.sendStatus(404);
         }
         try{await list_prod.destroy();
+          logger.info("deleted all images with prod id match");
          return  res.sendStatus(204);
       }catch(e){
+        logger.error("error->"+e);
           return res.sendStatus(400);
       }
 
@@ -463,7 +509,7 @@ const vUser = (username === name) ? true : false;
   }
 
       else {
-          
+        logger.error("access prod level check ---> forbidden");
           return res.status(401).json({msg: "not valid"});
       }
   }
